a20eb9f30b62d170d621b34ee17c67d90dd71fcc
Katherine Pryszlak <kepryzk@gmail.com>
2017-10-05 16:30:55 -0700

    starter code

---
+++ lab-katherine/src/reducer/card.js
@@ -0,0 +1,29 @@
+let initialState = {}
+
+export default (state=initialState, action) => {
+  let  {type, payload} = action
+
+  switch(type){
+  case 'CATEGORY_CREATE': return {...state, [payload.id]: []}
+  case 'CATEGORY_DELETE': return {...state, [payload.id]: null}
+
+  case 'CARD_CREATE':
+    let categoryCards = state[payload.categoryId]
+    return {...state, [payload.categoryId]: [...categoryCards, payload]}
+
+  case 'CARD_UPDATE':
+    let updateState = state
+    updateState[payload.categoryId] = updateState[payload.categoryId].map(card => {
+      if(card.id === payload.id) card = payload
+      return card;
+    })
+    return {...updateState}
+
+  case 'CARD_DELETE':
+    let deleteState = state
+    deleteState[payload.categoryId] = deleteState[payload.categoryId].filter(card => card.id !== payload.id)
+    return {...deleteState}
+
+  default: return state
+  }
+}
